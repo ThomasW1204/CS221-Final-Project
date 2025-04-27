f24b043421b8fee683446d4bfef8bf9df8c3b24b
Brianna Grayson <bgray18@uab.edu>
2025-04-27 16:24:15 -0500

    changed createFlashCards to createSet, created studySet folder and files that lets the user see the file they just created, terms, definitions, flashcards, and quizz for that set speicifically, and dashboard button

--- src/client/pages/createStudySet/createStudySet.js
+++ src/client/pages/createStudySet/createStudySet.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     const cardsContainer = document.getElementById("cardsContainer");
     const addCardbutton = document.getElementById("addCard");
     const exportSetBtn= document.getElementById("exportSet");
-    const viewFlashCardButton=document.getElementById("viewFlashcards");
+    const createStudySet=document.getElementById("createStudySet");
     const setTitleInput= document.getElementById("setTitle");
  
     //creates card DOM element
@@ -25,13 +25,13 @@ document.addEventListener("DOMContentLoaded",()=>{
             definitionTextArea.style.height=definitionTextArea.scrollHeight+"px";
         })
 
-        const imageInput=document.createElement("input");
-        imageInput.type="text";
-        imageInput.placeholder="Enter image URL (optional)";
+       // const imageInput=document.createElement("input");
+        //imageInput.type="text";
+        //imageInput.placeholder="Enter image URL (optional)";
 
         cardDiv.appendChild(termInput);
         cardDiv.appendChild(definitionTextArea);
-        cardDiv.appendChild(imageInput);
+       // cardDiv.appendChild(imageInput);
         
         return cardDiv;
     };
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded",()=>{
             return{
                 term:inputs[0].value.trim(),
                 definition: inputs[1].value.trim(),
-                image:inputs[2].value.trim()||null
+               // image:inputs[2].value.trim()||null
             };
         }).filter(card=> card.term && card.definition); //filter keeps the cards that have at least a term and definition
 
@@ -96,7 +96,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     });
 
     //view Flashcards via flashcardPage.html
-    viewFlashCardButton.addEventListener("click",()=>{
+    createStudySet.addEventListener("click",()=>{
 
 
         //collects flashCards if the user made some;returns if not
@@ -108,7 +108,7 @@ document.addEventListener("DOMContentLoaded",()=>{
 
         //helps flashcardPage.html know by setName which file to let the viewer see (the one just uploaded)
         localStorage.setItem("currentSet",flashCardSet.setName);
-        window.location.href ="flashcardPage.html";
+        window.location.href ="studySet.html";
     });
 
     //adds first card by default
